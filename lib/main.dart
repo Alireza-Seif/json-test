20254876cef9ba623cff1a7e267a9d72d8af79af
alirezaseif20 <alirezaariye@gmail.com>
2024-04-21 15:55:03 +0330

    feat: test one

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,57 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String title = 'loading...';
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Text(
+                title,
+                style: const TextStyle(fontSize: 28),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    getData();
+                  },
+                  child: const Text(
+                    'click to get data',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void getData() async {
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+
+    Response response = await get(uri);
+    setState(() {
+       title = jsonDecode(response.body)['title'];
+    });
   }
 }
