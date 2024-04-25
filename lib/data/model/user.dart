0c57aed75bd046680351a71fc5c2017b0f78e7d1
alirezaseif20 <alirezaariye@gmail.com>
2024-04-25 17:11:59 +0330

    feat: test four

---
+++ lib/data/model/user.dart
@@ -0,0 +1,5 @@
+class User {
+  int id;
+  String name, username, city, phone;
+  User(this.id, this.name, this.username, this.city, this.phone);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,18 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:json_test/screens/home_screen.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const Application());
 }
 
-class MainApp extends StatefulWidget {
-  MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  String title = 'loading...';
+class Application extends StatelessWidget {
+  const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Text(
-                title,
-                style: const TextStyle(fontSize: 28),
-              ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    getData();
-                  },
-                  child: const Text(
-                    'click to get data',
-                    style: TextStyle(fontSize: 28),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
-  }
-
-  void getData() async {
-    var uri = Uri.parse('https://jsonplaceholder.typicode.com/users');
-
-    Response response = await get(uri);
-    for (var i = 0; i < 10; i++) {
-      var name = jsonDecode(response.body)[i]['name'];
-      print(name);
-    }
   }
 }
